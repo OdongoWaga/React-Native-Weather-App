cc1a096680005902d236aac09ab3431fad30c573
Waga <wagaodongo@gmail.com>
2019-07-04 16:13:25 +0300

    search input

--- App.js
+++ App.js
@@ -1,34 +1,44 @@
 import React from 'react';
 import {
   StyleSheet,
+  View,
+  ImageBackground,
   Text,
   KeyboardAvoidingView,
   Platform,
-  TextInput,
 } from 'react-native';
+
+import getImageForWeather from './utils/getImageForWeather';
+
+import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
   render() {
+    const location ='Nairobi'
     return (
       <KeyboardAvoidingView
         style={styles.container}
         behavior="padding"
       >
-        <Text style={[styles.largeText, styles.textStyle]}>
-          San Francisco
-        </Text>
-        <Text style={[styles.smallText, styles.textStyle]}>
-          Light Cloud
-        </Text>
-        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+        <ImageBackground
+          source={getImageForWeather('Clear')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              {location}
+            </Text>
+            <Text style={[styles.smallText, styles.textStyle]}>
+              Light Cloud
+            </Text>
+            <Text style={[styles.largeText, styles.textStyle]}>
+              24°
+            </Text>
 
-        <TextInput
-          autoCorrect={false}
-          placeholder="Search any city"
-          placeholderTextColor="white"
-          style={styles.textInput}
-          clearButtonMode="always"
-        />
+            <SearchInput placeholder="Search any city" />
+          </View>
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
@@ -36,29 +46,33 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#34495E',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
   textStyle: {
     textAlign: 'center',
     fontFamily:
       Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
     fontSize: 44,
   },
   smallText: {
     fontSize: 18,
-  },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
   },
 });
